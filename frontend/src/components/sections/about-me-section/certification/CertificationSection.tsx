3f74ef04be24ddc15f6cbb36cbd14dd0a5453fba
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2025-10-22 14:06:52 -0300

    Update CertificationSection.tsx

--- frontend/src/components/sections/about-me-section/certification/CertificationSection.tsx
+++ frontend/src/components/sections/about-me-section/certification/CertificationSection.tsx
@@ -11,7 +11,7 @@ const CertificationSection = () => {
       title: t("certifications.codoACodo.title"),
       place: t("certifications.codoACodo.place"),
       image: cacImage,
-      pdf: "/portfolio/es/certifications/cac/cac-certificado.pdf",
+      pdf: "https://damrodriguez.github.io/portfolio/es/certifications/cac/cac-certificado.pdf",
     },
   ]
 
