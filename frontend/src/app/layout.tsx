3b9b7228ed2d66a80429a40f94eb4054a82099b2
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2025-11-01 20:10:00 -0300

    feat: metadata test

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,27 @@ import "@/styles/globals.css";
 export const metadata: Metadata = {
   title: "Portfolio - Damian Rodriguez",
   description: "Portfolio personal de Damian Rodriguez",
+  openGraph: {
+    title: "Portfolio - Damian Rodriguez",
+    description: "Portfolio personal de Damian Rodriguez",
+    url: process.env.NEXT_PUBLIC_BASE_PATH,
+    siteName: "Damian Rodriguez Portfolio",
+    images: [
+      {
+        url: "/code.png",
+        width: 1200,
+        height: 630,
+        alt: "Imagen de Damian Rodriguez Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio - Damian Rodriguez",
+    description: "Portfolio personal de Damian Rodriguez",
+    images: ["/code.png"],
+  },
 };
 
 type RootLayoutProps = {
