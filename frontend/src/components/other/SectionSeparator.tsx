15b113b5fcaf903909cddea9051b1cb52bcbcc07
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-03-02 23:23:16 -0300

    fix: separator bar

--- frontend/src/components/other/SectionSeparator.tsx
+++ frontend/src/components/other/SectionSeparator.tsx
@@ -1,19 +1,14 @@
 import clsx from "clsx";
 import MotionStretch from "@/components/motion/MotionStretch";
-import { BorderBeam } from "@/components/magic-ui/border-beam"
-import useIsMobile from "@/hooks/useIsMobile";
 
 type SectionSeparatorProps = {
   className?: string;
 };
 
 const SectionSeparator = ({ className }: SectionSeparatorProps) => {
-  const isMobile = useIsMobile();
   return (
     <MotionStretch>
-      <div className={clsx("h-[0.03125rem] w-full bg-soft-gray/30 relative", className)}>
-        <BorderBeam initialOffset={50} duration={8} size={isMobile ? 150 : 1000} colorFrom="var(--color-soft-gray/30)" colorTo="var(--color-opaque-gray)" />
-      </div>
+      <div className={clsx("h-[0.03125rem] w-full bg-soft-gray/30", className)} />
     </MotionStretch>
   );
 };
