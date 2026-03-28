3ee086677bc1613dc0436c206c4a7882386cb702
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-03-28 00:05:45 -0300

    fix: next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,6 @@ const isGithub = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true },
   basePath: isGithub ? "/portfolio" : "",
   assetPrefix: isGithub ? "/portfolio/" : "",
   trailingSlash: true,
