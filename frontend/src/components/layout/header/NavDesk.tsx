c5d24f071789de2dcd8af3707f3db6df0d3070a3
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-03-28 02:34:33 -0300

    fix: nav desk accesibility

--- frontend/src/components/layout/header/NavDesk.tsx
+++ frontend/src/components/layout/header/NavDesk.tsx
@@ -12,33 +12,35 @@ const NavDesk = () => {
   return (
     <nav className="hidden xl:flex">
       <ul className="text-lg leading-[1.5rem]">
-        <MotionStagger className="flex gap-[3rem]" direction="up" duration={0.3}>
-          {routeItems.map((item, index) => {
-            const isActive = activeSection === item.label;
-            const isOdd = index % 2 !== 0;
+        <li>
+          <MotionStagger className="flex gap-[3rem]" direction="up" duration={0.3}>
+            {routeItems.map((item, index) => {
+              const isActive = activeSection === item.label;
+              const isOdd = index % 2 !== 0;
 
-            return (
-              <li key={index} className={clsx("flex items-center justify-center flex-col group relative hover:text-black transition-all duration-600", {
-                "text-black": isActive,
-                "text-medium-gray": !isActive
-              })}>
-                <Link
-                  href={item.href}
-                  onClick={() => { }}
-                  className="cursor-pointer p-[0.5rem]"
-                >
-                  {t(item.label)}
-                </Link>
-                <div className={clsx("absolute -z-10 w-full h-full shadow-s1 bg-soft-white/90 transition-transform duration-600 scale-x-0 group-hover:scale-x-100 origin-center ease-in-out", {
-                  "scale-x-100": isActive,
-                  "rotate-5": !isOdd,
-                  "-rotate-5": isOdd
-                })}
-                />
-              </li>
-            );
-          })}
-        </MotionStagger>
+              return (
+                <div key={index} className={clsx("flex items-center justify-center flex-col group relative hover:text-black transition-all duration-600", {
+                  "text-black": isActive,
+                  "text-medium-gray": !isActive
+                })}>
+                  <Link
+                    href={item.href}
+                    onClick={() => { }}
+                    className="cursor-pointer p-[0.5rem]"
+                  >
+                    {t(item.label)}
+                  </Link>
+                  <div className={clsx("absolute -z-10 w-full h-full shadow-s1 bg-soft-white/90 transition-transform duration-600 scale-x-0 group-hover:scale-x-100 origin-center ease-in-out", {
+                    "scale-x-100": isActive,
+                    "rotate-5": !isOdd,
+                    "-rotate-5": isOdd
+                  })}
+                  />
+                </div>
+              );
+            })}
+          </MotionStagger>
+        </li>
       </ul>
     </nav>
   );
