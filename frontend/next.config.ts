192b23d0bf1a2cebacd7e5b5692848138aea6b09
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-03-28 00:10:02 -0300

    fix: next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const isGithub = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
-  output: "export",
   basePath: isGithub ? "/portfolio" : "",
   assetPrefix: isGithub ? "/portfolio/" : "",
   trailingSlash: true,
