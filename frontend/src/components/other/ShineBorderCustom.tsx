77b341b421e7bc5351666d0e68f79ec91f37a500
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-03-29 20:29:30 -0300

    fix: colors

--- frontend/src/components/other/ShineBorderCustom.tsx
+++ frontend/src/components/other/ShineBorderCustom.tsx
@@ -13,9 +13,9 @@ const ShineBorderCustom = (props: ShineBorderCustomProps) => {
   return (
     <ShineBorder
       className={clsx("", {
-        "border border-dark-gray/30 dark:border-soft-gray/15": props.isVisible,
+        "border border-dark-gray/30 dark:border-soft-gray/30": props.isVisible,
       })}
-      shineColor={theme === "dark" ? "rgba(255, 255, 255, 0.2)" : "rgba(0, 0, 0, 0.3)"}
+      shineColor={theme === "dark" ? "rgba(255, 255, 255, 0.3)" : "rgba(170, 170, 170, 0.5)"}
     />
   );
 };
