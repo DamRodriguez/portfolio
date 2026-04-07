e872b9ce90fb3affa296e5f574e766949add7117
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-04-07 17:53:04 -0300

    fix: chatbot styles

--- frontend/src/components/other/VoiceflowChat.tsx
+++ frontend/src/components/other/VoiceflowChat.tsx
@@ -32,12 +32,27 @@ declare global {
 }
 
 const VoiceflowChat = ({ locale }: { locale: string }) => {
-  // CSS Codificado:
-  // textarea { color-scheme: light !important; }
-  // [class*="vfrc-chat-input"] { color-scheme: light !important; }
-  // [class*="vfrc-chat-input-container"] { color-scheme: light !important; }
+  {/*
+    CSS Codificado:
+    textarea { color-scheme: light !important; }
 
-  const customCSSBase64 = "data:text/css;base64,dGV4dGFyZWEgeyBjb2xvci1zY2hlbWU6IGxpZ2h0ICFpbXBvcnRhbnQ7IH0KW2NsYXNzKj0idmZyYy1jaGF0LWlucHV0Il0geyBjb2xvci1zY2hlbWU6IGxpZ2h0ICFpbXBvcnRhbnQ7IH0KW2NsYXNzKj0idmZyYy1jaGF0LWlucHV0LWNvbnRhaW5lciIgeyBjb2xvci1zY2hlbWU6IGxpZ2h0ICFpbXBvcnRhbnQ7IH0=";
+    [class*="vfrc-button"] { 
+      box-shadow: 0 0 30px 0 rgba(255, 255, 255, 0.03) inset, 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.2), 0 1px 10px 0 rgba(0, 0, 0, 0.2) !important;
+      border: 1px solid rgba(255, 255, 255, 0.5) !important;
+    }
+      
+    [class*="vfrc-chat-dialog__container"] { 
+      box-shadow: 0 0 30px 0 rgba(255, 255, 255, 0.03) inset, 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.2), 0 1px 10px 0 rgba(0, 0, 0, 0.2) !important;
+      border: 1px solid rgba(255, 255, 255, 0.5) !important;
+    }
+
+    [class*="vfrc-header--button"] {
+      box-shadow: none !important;
+      border: none !important;
+    }
+  */}
+
+  const customCSSBase64 = "data:text/css;base64,dGV4dGFyZWEgeyBjb2xvci1zY2hlbWU6IGxpZ2h0ICFpbXBvcnRhbnQ7IH0KW2NsYXNzKj0idmZyYy1idXR0b24iXSwgW2NsYXNzKj0idmZyYy1jaGF0LWRpYWxvZ19fY29udGFpbmVyIl0geyBib3gtc2hhZG93OiAwIDAgMzBweCAwIHJnYmEoMjU1LCAyNTUsIDI1NSwgMC4wMykgaW5zZXQsIDAgMnB4IDRweCAwIHJnYmEoMCwgMCwgMCwgMC4yKSwgMCA0cHggNXB4IDAgcmdiYSgwLCAwLCAwLCAwLjIpLCAwIDFweCAxMHB4IDAgcmdiYSgwLCAwLCAwLCAwLjIpICFpbXBvcnRhbnQ7IGJvcmRlcjogMXB4IHNvbGlkIHJnYmEoMjU1LCAyNTUsIDI1NSwgMC41KSAhaW1wb3J0YW50OyB9CltjbGFzcyo9InZmcmMtaGVhZGVyLS1idXR0b24iXSB7IGJveC1zaGFkb3c6IG5vbmUgIWltcG9ydGFudDsgYm9yZGVyOiBub25lICFpbXBvcnRhbnQ7IH0=";
 
   return (
     <Script
