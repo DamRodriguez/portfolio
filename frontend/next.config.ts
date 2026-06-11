9dadf79928ddf8e0cac57249f785d1374e7cb424
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-06-11 16:40:34 -0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
