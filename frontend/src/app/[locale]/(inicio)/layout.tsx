d9da6c7e78459368117fa8f7ddcd6ae9d43d9a23
DamRodriguez <141194257+DamRodriguez@users.noreply.github.com>
2026-07-07 21:49:42 -0300

    Update layout.tsx

--- frontend/src/app/[locale]/(inicio)/layout.tsx
+++ frontend/src/app/[locale]/(inicio)/layout.tsx
@@ -13,7 +13,6 @@ export default async function HomeLayout({
   params,
 }: HomeLayoutProps) {
   const locale = await getValidatedLocale(params);
-
   return (
     <>
       <HomeHeader locale={locale} />
